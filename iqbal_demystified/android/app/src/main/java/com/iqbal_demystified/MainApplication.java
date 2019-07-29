ef1f6984bbf4cefdccea6d008981fd062b69c433
Ahmed Ghumman <ghumman@vt.edu>
2019-07-29 07:38:07 -0400

    started making change to make the project work with android

--- iqbal_demystified/android/app/src/main/java/com/iqbal_demystified/MainApplication.java
+++ iqbal_demystified/android/app/src/main/java/com/iqbal_demystified/MainApplication.java
@@ -3,8 +3,6 @@ package com.iqbal_demystified;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.RNFetchBlob.RNFetchBlobPackage;
-import com.guichaguri.trackplayer.TrackPlayer;
 import com.brentvatne.react.ReactVideoPackage;
 import li.yunqi.rnsecurestorage.RNSecureStoragePackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
@@ -30,8 +28,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNFetchBlobPackage(),
-            new TrackPlayer(),
             new ReactVideoPackage(),
             new RNSecureStoragePackage(),
             new AsyncStoragePackage(),
