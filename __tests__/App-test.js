bc85eabc7714cbb12417535c21d00f2a68ee9a95
Ahmed Ghumman <ghumman@vt.edu>
2020-02-13 05:27:18 -0500

    Added dummy tests and Removed test.js from src/screens/Misc/test.js

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -4,11 +4,43 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
+// import App from '../src/App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+/*
+// Following test does not pass
 it('renders correctly', () => {
   renderer.create(<App />);
 });
+*/
+
+expect.extend({
+  toBeWithinRange(received, floor, ceiling) {
+    const pass = received >= floor && received <= ceiling;
+    if (pass) {
+      return {
+        message: () =>
+          `expected ${received} not to be within range ${floor} - ${ceiling}`,
+        pass: true,
+      };
+    } else {
+      return {
+        message: () =>
+          `expected ${received} to be within range ${floor} - ${ceiling}`,
+        pass: false,
+      };
+    }
+  },
+});
+
+
+test('numeric ranges', () => {
+  expect(100).toBeWithinRange(90, 110);
+  expect(101).not.toBeWithinRange(0, 100);
+  expect({apples: 6, bananas: 3}).toEqual({
+    apples: expect.toBeWithinRange(1, 10),
+    bananas: expect.not.toBeWithinRange(11, 20),
+  });
+})
